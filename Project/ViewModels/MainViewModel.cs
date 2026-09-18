ad615f86bfca14b56e27003c4277f7c723341a58
lee <onitra35@gmail.com>
2026-09-18 23:31:55 +0900

    기초 UI

---
+++ Project/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.ViewModels
+{
+    public class MainViewModel
+    {
+        public string Title { get; set; } = "MediDesk";
+        public string PatientName { get; set; } = "김철수";
+
+    }
+}
